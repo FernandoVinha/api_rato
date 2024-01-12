7b6e72024aad7e09084c9d238981d7be04c0ddae
FernandoVinha <fernando.h.vinha@gmail.com>
2024-01-12 13:16:16 -0300

    Descrição das alterações

--- photo_trap/admin.py
+++ photo_trap/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import PhotoTrap
+from .models import PhotoTrap, Photo
 
 @admin.register(PhotoTrap)
 class PhotoTrapAdmin(admin.ModelAdmin):
-    list_display = ['mac_address', 'ntp_server', 'gmt_offset', 'daylight_offset', 'movement_sensor', 'restart', 'photo_shooting_frequency', 'latitude', 'longitude', 'floor', 'server_url', 'firmware_path', 'install_new_firmware']
+    list_display = ['mac_address', 'country', 'region', 'city', 'server_url']
+    search_fields = ['mac_address', 'country__name', 'city__name']
+    list_filter = ['country', 'region', 'city']
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ['photo_trap', 'capture_date', 'processed']
+    search_fields = ['photo_trap__mac_address', 'capture_date']
+    list_filter = ['photo_trap', 'capture_date', 'processed']
