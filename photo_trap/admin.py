a35de5502c58d3b02f3d8559df5a5dbea8eef2b9
FernandoVinha <fernando.h.vinha@gmail.com>
2024-01-12 17:26:17 -0300

    Descrição das alterações

--- photo_trap/admin.py
+++ photo_trap/admin.py
@@ -3,9 +3,12 @@ from .models import PhotoTrap, Photo
 
 @admin.register(PhotoTrap)
 class PhotoTrapAdmin(admin.ModelAdmin):
-    list_display = ['mac_address', 'country', 'region', 'city', 'server_url']
-    search_fields = ['mac_address', 'country__name', 'city__name']
-    list_filter = ['country', 'region', 'city']
+    list_display = [
+        'mac_address', 'country', 'region', 'city', 
+        'photo_url', 'firmware_url', 'latitude', 'longitude', 'floor'
+    ]
+    search_fields = ['mac_address', 'country__name', 'city__name', 'region__name']
+    list_filter = ['country', 'region', 'city', 'movement_sensor', 'restart', 'install_new_firmware']
 
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
